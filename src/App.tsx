416aa9d540a3bf0903f8711db225141a7b98ab57
atagan-deb <96320896+atagan-deb@users.noreply.github.com>
2021-12-31 11:48:20 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,13 @@
-import { useState } from "react";
 import "./styles.css";
 import { UserCard } from "./components/UserCard";
-import { UserProfile } from "./types/userProfile";
-import { User } from "./types/api/user";
-import axios from "axios";
+import { useAllUsers } from "./hooks/useAllUsers";
 
 export default function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<boolean>(false);
-  const onClickFetchData = () => {
-    setLoading(true);
-    setError(false);
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name} (${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => setLoading(false));
-  };
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+
   return (
     <div className="App">
-      <button onClick={onClickFetchData}>データ取得</button>
+      <button onClick={getUsers}>データ取得</button>
       <br />
       {error ? (
         <p style={{ color: "red" }}>データの取得に失敗しました。</p>
